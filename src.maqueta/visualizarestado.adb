e7c26ef9fa9ecf4fe1774085080e51a904d76785
rAknil <raknil@hotmail.com>
2010-10-04 12:15:59 +0200

    Puesto ejemplo en el módulo visualizarestado.
    Realización de un código de prueba a la hora de visualizar el estado
    en el menú.

--- src.maqueta/visualizarestado.adb
+++ src.maqueta/visualizarestado.adb
@@ -2,6 +2,17 @@ with Ada.Text_IO;
 package body VisualizarEstado is
     procedure MostrarEstado is
     begin
-        Ada.Text_IO.Put_Line("");
+        Ada.Text_IO.Put_Line("                   _ _ _ _          _ _ _ _");
+        Ada.Text_IO.Put_Line("                  |_|_|_|_| Fila 1 |_|_|_|_|");
+        Ada.Text_IO.Put_Line("                  |_|_|_|_| Fila 2 |X|_|_|_|");
+        Ada.Text_IO.Put_Line("                   1 2 3 4          5 6 7 8");
+        Ada.Text_IO.Put_Line("    ______             _ _          _ _             ______");
+        Ada.Text_IO.Put_Line("   |      |P4        _|_|_|   F4   |_|_|_          |      |P3");
+        Ada.Text_IO.Put_Line("   |______|        _|_|X|X|   F3   |_|_|X|_        |______|");
+        Ada.Text_IO.Put_Line("   |      |P2     |_|_|_|_|   F2   |O|O|O|_|       |      |P1");
+        Ada.Text_IO.Put_Line("   |______|       |_|_|_|_|   F1   |_|_|_|_|       |______|");
+        Ada.Text_IO.Put_Line("                   1 2 3 4          5 6 7 8");
+        Ada.Text_IO.Put_Line("                                              X -> Comprado");
+        Ada.Text_IO.Put_Line("                                              O -> Reservado");
     end MostrarEstado;
 end VisualizarEstado;
