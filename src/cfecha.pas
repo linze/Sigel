cbdb755784942d7e256cf2080073c61077fb0ebd
Francisco Canela <darkydelphi@gmail.com>
2010-10-30 14:36:21 +0200

    Formateados TEstadoLocalidad,TFecha y TTipoLocalidad
    A cuatro espacios.

--- src/cfecha.pas
+++ src/cfecha.pas
@@ -3,13 +3,12 @@ unit CFecha;
 {$mode objfpc}{$H+}
 
 interface
-         type
-             TFecha = record
-                    Dia : 1..31;
-                    Mes : 1..12;
-             end;
+    type
+        TFecha = record
+            Dia : 1..31;
+            Mes : 1..12;
+        end;
 
 implementation
-
 end.
 
