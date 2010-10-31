2273a367ee1aa2b7fee26a8ff9c1a8c8f343d866
Francisco Canela <darkydelphi@gmail.com>
2010-10-31 12:48:37 +0100

    Añadidos formularios vacios para Compra y Reserva

---
+++ src/comprarfrm.pas
@@ -0,0 +1,27 @@
+unit ComprarFrm; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs; 
+
+type
+  TfrmComprar = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  frmComprar: TfrmComprar;
+
+implementation
+
+initialization
+  {$I comprarfrm.lrs}
+
+end.
+
