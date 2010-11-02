0d7585f271c6b41d5eda3927b74d9544471f2959
Javier Benito <raknil@hotmail.com>
2010-11-02 21:29:20 +0100

    Comienzo de la realización de la interfaz gráfica en principalfrm.pas

--- src/principalfrm.pas
+++ src/principalfrm.pas
@@ -5,10 +5,26 @@ unit PrincipalFrm;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, StdCtrls;
 
 type
+
+  { TfrmPrincipal }
+
   TfrmPrincipal = class(TForm)
+      Button1: TButton;
+      Button2: TButton;
+      Button3: TButton;
+      Button4: TButton;
+      Image1: TImage;
+      Image2: TImage;
+      StaticText1: TStaticText;
+      StaticText2: TStaticText;
+      StaticText3: TStaticText;
+      procedure FormCreate(Sender: TObject);
+      procedure Image2Click(Sender: TObject);
+      procedure StaticText1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -19,6 +35,23 @@ var
   frmPrincipal: TfrmPrincipal;
 
 implementation
+
+{ TfrmPrincipal }
+
+procedure TfrmPrincipal.StaticText1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TfrmPrincipal.Image2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TfrmPrincipal.FormCreate(Sender: TObject);
+begin
+
+end;
 
 initialization
   {$I principalfrm.lrs}
