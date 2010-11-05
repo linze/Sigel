1c64ead96379b0d72f7eb28305a17e53e9d87dc0
Francisco Canela <darkydelphi@gmail.com>
2010-11-05 16:33:57 +0100

    Añadido frmDatosEspera

--- src/datosespera.pas
+++ src/datosespera.pas
@@ -1,14 +1,34 @@
-unit DatosEspera; 
+unit DatosEspera;
 
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, StdCtrls, Spin;
 
 type
+
+  { TfrmDatosEspera }
+
   TfrmDatosEspera = class(TForm)
+      btnAceptar: TButton;
+      btnCancelar: TButton;
+      cbTipo: TComboBox;
+      eNombre: TEdit;
+      eTelefono: TEdit;
+      eEmail: TEdit;
+      GroupBox1: TGroupBox;
+      GroupBox2: TGroupBox;
+      Label1: TLabel;
+      Label2: TLabel;
+      Label3: TLabel;
+      Label4: TLabel;
+      Label5: TLabel;
+      Label6: TLabel;
+      seCantidad: TSpinEdit;
+      procedure GroupBox2Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -19,6 +39,13 @@ var
   frmDatosEspera: TfrmDatosEspera;
 
 implementation
+
+{ TfrmDatosEspera }
+
+procedure TfrmDatosEspera.GroupBox2Click(Sender: TObject);
+begin
+
+end;
 
 initialization
   {$I datosespera.lrs}
